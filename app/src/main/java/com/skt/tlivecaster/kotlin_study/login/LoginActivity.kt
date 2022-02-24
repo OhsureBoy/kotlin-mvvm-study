446d823986b45ff28376780b6fc530083ae2aab2
Zisouu <47763201+Zisouu@users.noreply.github.com>
2022-02-24 18:16:06 +0900

    first Project

---
+++ app/src/main/java/com/skt/tlivecaster/kotlin_study/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.skt.tlivecaster.kotlin_study.login
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.skt.tlivecaster.kotlin_study.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/skt/tlivecaster/kotlin_study/login/loginViewModel.kt
@@ -0,0 +1,6 @@
+package com.skt.tlivecaster.kotlin_study.login
+
+class loginViewModel {
+
+
+}
